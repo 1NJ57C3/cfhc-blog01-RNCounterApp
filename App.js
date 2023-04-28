a345d9ce6b0e31777e3ee4f9d55a7e97857f7b2d
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2023-04-28 11:15:20 -0700

    Git initialized

---
+++ App.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+
+import React from 'react';
+import {} from 'react-native';
+
+function App() {
+  return (
+    <React.Fragment />
+  );
+}
+
+export default App;
